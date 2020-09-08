0e45f6a96c4bb7168bbb05eb3e2fce32b8803a10
Olya Polishchuk <polishchukov2015@gmail.com>
2020-09-08 22:31:26 +0300

    -Write a Java program to print an American flag on the screen

---
+++ src/main/java/org/example/basic1/exercise014/Exercise014.java
@@ -0,0 +1,25 @@
+package org.example.basic1.exercise014;
+
+public class Exercise014 {
+    public static void main (String[] args){
+        int americanFlag = americanFlag();
+    }
+    public static int americanFlag() {
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        return 0;
+    }
+}
