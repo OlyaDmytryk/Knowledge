f2e9594b5929f8473bd8e7c75792f8d16d1a84a0
Olya Polishchuk <polishchukov2015@gmail.com>
2020-09-08 22:32:57 +0300

    -Write a Java program to print a face.

---
+++ src/main/java/org/example/basic1/exercise016/Exercise016.java
@@ -0,0 +1,15 @@
+package org.example.basic1.exercise016;
+
+public class Exercise016 {
+    public static void main(String[] args) {
+        int face = face();
+    }
+    public static int face() {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+        return 0;
+    }
+}
