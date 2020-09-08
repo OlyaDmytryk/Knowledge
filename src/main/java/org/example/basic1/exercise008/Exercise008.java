a60b3c4613e61dd7a48e5296005b42c636dc3dbb
Olya Polishchuk <polishchukov2015@gmail.com>
2020-09-08 22:28:56 +0300

    -Write a Java program to display the following pattern

---
+++ src/main/java/org/example/basic1/exercise008/Exercise008.java
@@ -0,0 +1,14 @@
+package org.example.basic1.exercise008;
+
+public class Exercise008 {
+    public static void main(String[] args) {
+        int javaWithLetters = javaWithLetters();
+    }
+    public static int javaWithLetters(){
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+        return 0;
+    }
+}
