91e7ad59867f11070ad7e51e7bd29a3dc0cc873d
Olya Polishchuk <polishchukov2015@gmail.com>
2020-10-04 13:28:22 +0300

    -Write a Java program that reads a number in inches, converts it to meters.

---
+++ src/main/java/dataTypes/exercise02/Exercise02.java
@@ -0,0 +1,15 @@
+package dataTypes.exercise02;
+
+public class Exercise02 {
+    public static void main(String[] args){
+        int convertNumber = convertNumber();
+    }
+    public static int convertNumber() {
+        double inch = 1000.0;
+        System.out.println("One inch is 0.0254 meter");
+        System.out.println("Value for inch: " + inch);
+        double meters = inch * 0.0254;
+        System.out.println(inch + "inch is " + meters + " meters");
+        return 0;
+    }
+}
