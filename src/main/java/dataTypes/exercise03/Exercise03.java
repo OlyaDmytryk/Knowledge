5d8d6d6c034c8f186e275647a5afc4805a7b36d6
Olya Polishchuk <polishchukov2015@gmail.com>
2020-10-04 13:43:57 +0300

    -Write a Java program that reads an integer between 0 and 1000 and adds all the digits in the integer.

---
+++ src/main/java/dataTypes/exercise03/Exercise03.java
@@ -0,0 +1,21 @@
+package dataTypes.exercise03;
+
+public class Exercise03 {
+    public static void main(String[] args){
+        int addAllDigits = addAllDigits();
+    }
+    public static int addAllDigits(){
+        int integer = 565;
+        System.out.println("Integer between 0 and 1000:  " + integer);
+        int fistNumber = integer % 10;
+        int remainingNumber = integer / 10;
+        int secondNumber = remainingNumber % 10;
+        remainingNumber = remainingNumber / 10;
+        int thirdNumber = remainingNumber % 10;
+        remainingNumber = remainingNumber / 10;
+        int fourthNumber = remainingNumber % 10;
+        int sum = fistNumber + secondNumber + thirdNumber + fourthNumber;
+        System.out.println("The  sum of all digits " + sum);
+        return 0;
+    }
+}
