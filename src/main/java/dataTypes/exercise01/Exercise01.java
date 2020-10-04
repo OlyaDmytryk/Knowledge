e3fb4535e9674cc6cd2ab83415f5e475d3116f89
Olya Polishchuk <polishchukov2015@gmail.com>
2020-10-04 13:28:07 +0300

    -Write a Java program to convert temperature from Fahrenheit to Celsius degree

---
+++ src/main/java/dataTypes/exercise01/Exercise01.java
@@ -0,0 +1,14 @@
+package dataTypes.exercise01;
+
+public class Exercise01 {
+    public static void main(String[] args){
+        int temperature = temperature();
+    }
+    public static int temperature(){
+       double temperatureOfFahrenheit = 212.0;
+       System.out.println("Degree in Fahrenheit: " + temperatureOfFahrenheit);
+       double temperatureOfCelsius = (temperatureOfFahrenheit - 32) / 1.8;
+       System.out.println(temperatureOfFahrenheit + "degree Fahrenheit is equal to " + temperatureOfCelsius + " in Celsius");
+       return  0;
+    }
+}
