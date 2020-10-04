1bf015090675e505f11a2e747783dc4f0769ed33
Olya Polishchuk <polishchukov2015@gmail.com>
2020-10-04 15:46:55 +0300

    -Write a Java program to compute body mass index (BMI).

---
+++ src/main/java/dataTypes/exercise06/Exercise06.java
@@ -0,0 +1,16 @@
+package dataTypes.exercise06;
+
+public class Exercise06 {
+    public static void main(String[] args){
+        int bodyIndex = bodyIndex();
+    }
+    public static int bodyIndex(){
+        int weight = 64;
+        double height = 1.65;
+        System.out.println("Weight: " + weight);
+        System.out.println("Height: " + height);
+        double index = weight / (height * height);
+        System.out.println("Index:" + index);
+        return 0;
+    }
+}
